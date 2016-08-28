41310e10c94cb3b92c71bdb3f57d599fd0281788
brc <brownchen7s@gmail.com>
2016-08-28 22:58:04 +0800

    碎片最佳实例

---
+++ FragmentBestPractice/app/src/main/java/com/example/brown/fragmentbestpractice/News.java
@@ -0,0 +1,27 @@
+package com.example.brown.fragmentbestpractice;
+
+/**
+ * Created by brown on 2016/8/28.
+ */
+public class News {
+
+    private String title;
+
+    private String content;
+
+    private String getTitle(){
+        return  title;
+    }
+
+    private void setTitle(String title){
+       this.title = title;
+    }
+
+    private String getContent(){
+        return content;
+    }
+
+    private void setContent(String content){
+        this.content = content;
+    }
+}
