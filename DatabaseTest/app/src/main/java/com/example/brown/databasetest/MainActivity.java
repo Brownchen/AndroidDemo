6375ce6c0675d65204491cb6e3959c7077d5a954
brc <brownchen7s@gmail.com>
2016-09-12 22:40:23 +0800

    添加数据

--- DatabaseTest/app/src/main/java/com/example/brown/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/brown/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.brown.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",19.95);
+                db.insert("Book",null,values);
+                values.clear();
+
+                values.put("name","The Last Symbol");
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",16.96);
+                db.insert("Book",null,values);
             }
         });
     }
