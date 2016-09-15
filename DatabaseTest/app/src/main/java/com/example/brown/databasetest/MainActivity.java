7dcae58e54c5d47cec77908432c54d06cdb4dcdd
brc <brownchen7s@gmail.com>
2016-09-15 16:43:24 +0800

    SQLite故障待查

--- DatabaseTest/app/src/main/java/com/example/brown/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/brown/databasetest/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.brown.databasetest;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private MyDatabaseHelper dbHelper;
 
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 dbHelper.getWritableDatabase();
             }
         });
+
         Button addData = (Button) findViewById(R.id.add_data);
         addData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,15 +34,26 @@ public class MainActivity extends AppCompatActivity {
                 values.put("name","The Da Vinci Code");
                 values.put("author","Dan Brown");
                 values.put("pages",454);
-                values.put("price",19.95);
-                db.insert("Book",null,values);
+                values.put("price",16.96);
+                db.insert("book",null,values);
                 values.clear();
 
                 values.put("name","The Last Symbol");
                 values.put("author","Dan Brown");
                 values.put("pages",510);
-                values.put("price",16.96);
-                db.insert("Book",null,values);
+                values.put("price",19.95);
+                db.insert("book",null,values);
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price",10.99);
+                db.update("book",values,"name=?",new String[] {"The Da Vinci Code"});
             }
         });
     }
--- DatabaseTest/app/src/main/java/com/example/brown/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/brown/databasetest/MyDatabaseHelper.java
@@ -11,11 +11,11 @@ import android.widget.Toast;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String CREATE_BOOK = "create table book ("
-            +"id integer primary key autoincrement,"
-            +"author text,"
-            +"price real"
-            +"pages integer"
-            +"name text)";
+            + "id integer primary key autoincrement, "
+            + "author text, "
+            + "price real, "
+            + "pages integer, "
+            + "name text)";
 
     public static final String CREATE_CATEGORY = "create table Category ("
             +"id integer primary key autoincrement,"
@@ -36,7 +36,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
-        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists book");
         db.execSQL("drop table if exists Category");
         onCreate(db);
 
