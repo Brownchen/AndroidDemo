8088cc950d2b71e9bdb6b3856f005acdce43dc41
brc <brownchen7s@gmail.com>
2016-09-18 21:47:16 +0800

    完成更新数据（重写后故障消除。。）

--- DatabaseTest/app/src/main/java/com/example/brown/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/brown/databasetest/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.brown.databasetest;
 
-import android.app.Activity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private MyDatabaseHelper dbHelper;
 
@@ -16,44 +16,40 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelper = new MyDatabaseHelper(this,"BookStore.db",null,2);
-        Button createDatabase = (Button) findViewById(R.id.create_database);
-        createDatabase.setOnClickListener(new View.OnClickListener() {
+        Button createDatabaseHelper = (Button) findViewById(R.id.create_database);
+        createDatabaseHelper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
             }
         });
-
-        Button addData = (Button) findViewById(R.id.add_data);
+        Button addData = (Button)findViewById(R.id.add_data);
         addData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
-
                 values.put("name","The Da Vinci Code");
                 values.put("author","Dan Brown");
                 values.put("pages",454);
-                values.put("price",16.96);
-                db.insert("book",null,values);
+                values.put("price",16.69);
+                db.insert("Book",null,values);
                 values.clear();
-
-                values.put("name","The Last Symbol");
+                values.put("name","The Lost Symbol");
                 values.put("author","Dan Brown");
-                values.put("pages",510);
+                values.put("pages" ,510);
                 values.put("price",19.95);
-                db.insert("book",null,values);
+                db.insert("Book",null,values);
             }
         });
-
-        Button updateData = (Button) findViewById(R.id.update_data);
+        Button updateData = (Button)findViewById(R.id.update_data);
         updateData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
                 values.put("price",10.99);
-                db.update("book",values,"name=?",new String[] {"The Da Vinci Code"});
+                db.update("Book",values,"name = ?",new String[] {"The Da Vinci Code"});
             }
         });
     }
