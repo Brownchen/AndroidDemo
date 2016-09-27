7f2dc4dc56340b7a45158db0f89b0e60ab6de8d9
brc <brownchen7s@gmail.com>
2016-09-27 20:37:03 +0800

    更正UIR以及MIME参数

--- DatabaseTest/app/src/main/java/com/example/brown/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/brown/databasetest/MyDatabaseHelper.java
@@ -3,7 +3,6 @@ package com.example.brown.databasetest;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by brown on 2016/9/18.
@@ -33,7 +32,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db){
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
     public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
