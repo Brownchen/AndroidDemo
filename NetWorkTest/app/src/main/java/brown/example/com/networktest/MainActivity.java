589f260ec25676d4ab9e6dfd8c671b7c94e6bbf7
brc <brownchen7s@gmail.com>
2016-11-16 21:19:39 +0800

    使用pull解析XML文件

--- NetWorkTest/app/src/main/java/brown/example/com/networktest/MainActivity.java
+++ NetWorkTest/app/src/main/java/brown/example/com/networktest/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -54,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 HttpURLConnection connection = null;
                 try {
-                    URL url = new URL("http://www.baidu.com");
+                    URL url = new URL("http://10.0.2.2:81/get_data.xml");
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
@@ -66,10 +71,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     while((line = reader.readLine()) != null){
                         response.append(line);
                     }
-                    Message msg = new Message();
-                    msg.what = SHOW_REQUEST;
-                    msg.obj = response.toString();
-                    handler.sendMessage(msg);
+                    parseXMLWithPull(response);
+//                    Message msg = new Message();
+//                    msg.what = SHOW_REQUEST;
+//                    msg.obj = response.toString();
+//                    handler.sendMessage(msg);
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
@@ -79,5 +85,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }).start();
+    }
+
+    private void parseXMLWithPull(StringBuilder response){
+        try {
+            String xmlData = response.toString();
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = " ";
+            String name = " ";
+            String version = " ";
+            while(eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    case XmlPullParser.START_TAG:{
+                        if ("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        } else if("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        }else if ("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG:{
+                        if ("app".equals(nodeName)){
+                            Log.d("MainActivity","id is" + id);
+                            Log.d("MainActivity","name is" + name);
+                            Log.d("MainActivity","version is" + version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+              eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
